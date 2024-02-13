b52fa02e38a87104dbdba6deb19c40a710242bd5
LauDuhalde <duhalde.ulloa.laura@gmail.com>
2024-02-13 09:14:07 -0300

    README + navbar y smooth scroll

---
+++ README.md
@@ -0,0 +1,32 @@
+#Prueba M2 - Fundamentos del desarrollo front-end: Viajes Chile
+##Autor: Laura Duhalde
+##URL GitHub: https://github.com/LauDuhalde/LauDuhalde.github.io
+
+##Estructura de archivos
+viajesChile/
+|_index.html
+|_assets/
+    |_css/
+        |_style.css
+    |_img/
+        |_card1.jpg
+        |_card2.jpg
+        |_card3.jpg
+        |_card4.jpg
+        |_carousel1.jpg
+        |_carousel2.jpg
+        |_carousel3.jpg
+        |_viajes.svg
+    |_js/
+        |_index.js
+
+##Bibliotecas utilizadas
+Fontawesome
+Google Font, fuente Montserrat
+Bootstrap 5.3.2
+Jquery 3.7.1
+Popper 2.11.8
+
+##Componentes utilizados
+Navbar
+Smooth scroll
---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+//Para tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$(document).ready(function () {
+  /*Smooth scroll - Selector por clase*/
+  $(".nav-link").click('click', function (event) {
+    if (this.hash !== "") {     //Mientras href tenga valor
+      event.preventDefault();   //Evita el comportamiento por defecto (salto al enlace)
+      var hash = this.hash;     //Asigna el valor de href a hash
+      /*selector por etiqueta*/
+      $('html, body').animate({ //Le da animación de desplazamiento en 8 segundos
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+        window.location.hash = hash; //Se desplaza
+      });
+    }
+  });
+
+});
